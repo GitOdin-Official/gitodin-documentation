57493efc431a53db58332b57c36ddcc8284ce3ec
Jonathan White <ThinkDigitalRepair@gmail.com>
2018-11-07 17:55:27 +0000

    Updated spelling and grammar

--- getting-started/what-is-git-odin.blade.php
+++ getting-started/what-is-git-odin.blade.php
@@ -1,8 +1,8 @@
 <p>In fancy terms, we are a Hosted Realtime Communications API provider.<br>
 <br>
-What does that mean? Simply put, to be able to integrate realtime into your application you will require a relay. In most cases, managing such a server
-can be quiet a headache and annoying; thus companies like Pusher are created. I've used companies like them for awhile. When I simply couldn't justify it anymore, I built my own network with intelligence for my projects. 
-Our infastructure allows us to scale vertically and/or horizonally on the fly, thus ensuring no matter the load the performance will always be optimized. <br>
-<br>In short, your client triggers a HTTPS requst to one of our Regional Towers which is known as a Ping Request. We process it and broadcast it to your listens which in turn allow for realtime updating.
-GitOdin provides you a secure method of integrating realtime in your games, maps, software applications and web applications with ease that scale when you scale.<br><br>
+What does that mean? Simply put, to be able to integrate real-time into your application you will require a relay. In most cases, managing such a server
+can be quite a headache and annoying; thus companies like Pusher are created. I've used companies like them for a while. When I simply couldn't justify it anymore, I built my own network with intelligence for my projects.
+Our infrastructure allows us to scale vertically and/or horizontally on the fly, thus ensuring no matter the load the performance will always be optimized. <br>
+<br>In short, your client triggers an HTTPS request to one of our Regional Towers which is known as a Ping Request. We process it and broadcast it to your listeners which in turn allow for real-time updating.
+GitOdin provides you a secure method of integrating real-time in your games, maps, software applications and web applications with ease that scales when you scale.<br><br>
 Give it a try, it's free.<br>Because we believe in <b>you</b>.</p>
